c735611d1920dd936fa62a7ef3cc2522b889963b
Sachin Duhan <sachin.webarachne@gmail.com>
2024-05-22 22:35:25 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.error("MONGODB connection error", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+/*
+import express from "express"; 
+const app = express();
+
+;(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, ()=> {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error
+    }
+})()
+*/

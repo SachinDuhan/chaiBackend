836111f1a41e298cc30338799da0146cafe92b40
Sachin Duhan <sachin.webarachne@gmail.com>
2024-07-04 00:33:09 +0530

    logout bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -149,8 +149,8 @@ const logoutUser = asyncHandler( async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {

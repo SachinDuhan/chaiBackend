2d567931c7ab7f5b01b8bab85d9022b21250f6c0
Sachin Duhan <sachin.webarachne@gmail.com>
2024-08-31 09:58:23 +0530

    added getVideoComments functionality

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,6 +9,28 @@ const getVideoComments = asyncHandler(async (req, res) => {
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
 
+    try {
+            const comments = Comment.aggregate([
+                {
+                    $match: {video: videoId}
+                }
+            ])
+        
+            const commentsPaginate = await Comment.aggregatePaginate(comments, {page: parseInt(page), limit: parseInt(limit)})
+
+            if (commentsPaginate.docs.length == 0) {
+                return res.status(200).json(new ApiResponse(200, commentsPaginate, "No Comments"))
+            }
+
+            if (!commentsPaginate) {
+                throw new ApiError(500, "Comments could not be loaded")
+            }
+
+            return res.status(200).json(new ApiResponse(200, commentsPaginate, "Comments loaded successfully"));
+    } catch (error) {
+        throw new ApiError(500, error.message, error)
+    }
+
 })
 
 const addComment = asyncHandler(async (req, res) => {
